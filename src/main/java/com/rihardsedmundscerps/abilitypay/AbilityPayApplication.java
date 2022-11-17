5107c273a48afcefdded55ae3b25c28fdad947bb
Rihards Edmunds Cerps <goldminelv@gmail.com>
2022-11-17 17:36:03 +0200

    Feat: Renamed main class

--- src/main/java/com/rihardsedmundscerps/abilitypay/AbilityPayApplication.java
+++ src/main/java/com/rihardsedmundscerps/abilitypay/AbilityPayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AbilitypayApplication {
+public class AbilityPayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AbilitypayApplication.class, args);
+		SpringApplication.run(AbilityPayApplication.class, args);
 	}
 
 }
