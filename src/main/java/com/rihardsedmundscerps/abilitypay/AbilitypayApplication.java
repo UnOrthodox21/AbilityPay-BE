5eefa12de7be8a74a1c22f44112806a00df09bd1
Rihards Edmunds Cerps <goldminelv@gmail.com>
2022-11-17 17:25:53 +0200

    Feat: Create AbilityPay Spring Boot project

---
+++ src/main/java/com/rihardsedmundscerps/abilitypay/AbilitypayApplication.java
@@ -0,0 +1,13 @@
+package com.rihardsedmundscerps.abilitypay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbilitypayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbilitypayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rihardsedmundscerps/abilitypay/AbilitypayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rihardsedmundscerps.abilitypay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbilitypayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
